49483606bf7dc73aec970768d852cf42f67b34c9
1.21.1搬运模组 <407334450@qq.com>
2026-04-10 11:26:12 +0800

    2次提交

--- src/main/java/net/mcreator/mofusbetterend/procedures/NautilusSubEntityIsHurtProcedure.java
+++
@@ -1,7 +0,0 @@
-package net.mcreator.mofusbetterend.procedures;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.damagesource.DamageSource;
-public class NautilusSubEntityIsHurtProcedure {
-    public static void execute(LevelAccessor world, Entity entity, DamageSource source) {}
-}
--- src/main/java/net/mcreator/mofusbetterend/procedures/NautilusSubOnEntityTickUpdateProcedure.java
+++
@@ -1,6 +0,0 @@
-package net.mcreator.mofusbetterend.procedures;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.entity.Entity;
-public class NautilusSubOnEntityTickUpdateProcedure {
-    public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {}
-}
